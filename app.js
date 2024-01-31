e2a32bf32add3127215486136c1cd1c4ccc63b1c
Santi <santiagomoreira70@gmail.com>
2024-01-31 08:38:02 -0300

    UPDATE API

--- app.js
+++ app.js
@@ -20,28 +20,22 @@ fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
 })
 .catch(error => console.error('Error al obtener comentarios:', error));
 
-//tiemporeal
 
-// Hacer la solicitud a la API
-fetch('http://worldtimeapi.org/api/timezone/America/Montevideo')
+fetch('https://worldtimeapi.org/api/timezone/America/Montevideo')
   .then(response => response.json())
   .then(data => {
-    // Acceder al contenedor
     const infoContainer = document.getElementById('infoContainer');
 
-    // Crear un string con la información que deseas mostrar
     const infoString = `
       <p>Fecha y hora: ${data.datetime}</p>
       <p>Zona horaria: ${data.timezone}</p>
       <p>Offset UTC: ${data.utc_offset}</p>
     `;
 
-    // Insertar el string en el contenedor
     infoContainer.innerHTML = infoString;
   })
   .catch(error => console.error('Error al obtener datos:', error));
 
   window.onload = function() {
-    // Tu código aquí
   }
   
